627dfa2a3c2420209e2ea25c14849102ef26c020
tsirkin <tsirkin@gmail.com>
2018-01-14 15:05:41 +0200

    more on load work

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,6 +1,48 @@
 // React when a browser action's icon is clicked.
-chrome.browserAction.onClicked.addListener(function(tab) {
+
+function injectRtl(){
     chrome.tabs.insertCSS({file:"rtl.css"});
     chrome.tabs.executeScript(null, {file: 'scripts/load.js'});
-    
+}
+
+const JiraURLsKey = "JiraURLsKey";
+// onDOMContentLoaded?
+chrome.webNavigation.onCompleted.addListener(function(tab) {
+    var url = tab.url;
+    var oUrl = new URL(url)
+    var domain = oUrl.hostname;
+    var bkg = chrome.extension.getBackgroundPage();
+    chrome.storage.sync.get(JiraURLsKey,function(urls) {
+	if (Object.keys(urls).length > 0) {
+	    // Already there
+	    alert(urls[domain]);
+	    if(!urls[domain]) return;
+	} 
+        bkg.console.log('Url in list');
+	injectRtl();
+    });
+});
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    var url = tab.url;
+    var oUrl = new URL(url)
+    var domain = oUrl.hostname;
+    // TODO: save the url
+    chrome.storage.sync.get(JiraURLsKey,function(urls) {
+	if (Object.keys(urls).length > 0) {
+	    // Already there
+	    if(urls[domain]) return;
+	} else {
+            // The data array doesn't exist yet, create it
+            urls = {};
+	}
+	urls[domain] = true;
+	let storage = {};
+	storage[JiraURLsKey] = urls;
+	// Now save the updated items using set
+	chrome.storage.sync.set(storage, function() {
+            //alert('Url successfully saved');
+	});
+    });
+    injectRtl();
 });
