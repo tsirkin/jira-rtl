1a4c3212984b086e959b51b0e7b9621e2649241f
Evgeny Tsirkin <tsirkin@gmail.com>
2019-06-24 10:42:28 +0300

    fixed formatting

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -16,7 +16,7 @@
     along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
  */
 
- require('../css/popup.css');
+require('../css/popup.css');
 require('../../node_modules/bootstrap-switch/dist/css/bootstrap3/bootstrap-switch.css');
 var $ = require("jquery");
 require("bootstrap-switch");
@@ -24,37 +24,37 @@ var JiraRtlController = require('./jira-rtl-controller');
 
 // Called when the user clicks on the browser action.
 document.addEventListener('DOMContentLoaded', function () {
-    console.log("popup content loaded");
-    function switchRtl(ev,state){
-	let ctl = new JiraRtlController();
-	if(state){
-	    ctl.execInCurrentDomain(async function(domain){
-		// console.log("Storing url : ",domain);
-		await ctl.storeUrl(domain);
-		ctl.injectRtl();
-		ctl.drawIcon()
-	    });
-	}else{
-	    ctl.execInCurrentDomain(async function(domain){
-		await ctl.removeUrl(domain);
-		ctl.drawIcon()
-	    });
+	console.log("popup content loaded");
+	function switchRtl(ev, state) {
+		let ctl = new JiraRtlController();
+		if (state) {
+			ctl.execInCurrentDomain(async function (domain) {
+				// console.log("Storing url : ",domain);
+				await ctl.storeUrl(domain);
+				ctl.injectRtl();
+				ctl.drawIcon()
+			});
+		} else {
+			ctl.execInCurrentDomain(async function (domain) {
+				await ctl.removeUrl(domain);
+				ctl.drawIcon()
+			});
+		}
 	}
-    }
-    var toggleBtn = $("#enable-jira-rtl");
-    let ctl = new JiraRtlController();
-    ctl.checkCurrentUrl((isUrlActive,currentHostname) => {
-	console.log("Showing toggle button with state ",isUrlActive);
-	// Note that the button text/color is the opposite from the default
-	// as showing 'On' means that the plugin is Off for the current moment.
-	toggleBtn.bootstrapSwitch({
-	    state: isUrlActive,
-	    size:'small',
-	    onColor:'success',
-	    onText:'ON',
-	    offColor:'default',
-	    offText:'OFF',
-	    onSwitchChange:switchRtl
+	var toggleBtn = $("#enable-jira-rtl");
+	let ctl = new JiraRtlController();
+	ctl.checkCurrentUrl((isUrlActive, currentHostname) => {
+		console.log("Showing toggle button with state ", isUrlActive);
+		// Note that the button text/color is the opposite from the default
+		// as showing 'On' means that the plugin is Off for the current moment.
+		toggleBtn.bootstrapSwitch({
+			state: isUrlActive,
+			size: 'small',
+			onColor: 'success',
+			onText: 'ON',
+			offColor: 'default',
+			offText: 'OFF',
+			onSwitchChange: switchRtl
+		});
 	});
-    });
 });
